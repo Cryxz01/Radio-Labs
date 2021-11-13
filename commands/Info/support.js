2695d8fab1db78c0c17e88ce0061db9502dcb920
wezah <78669985+wezah@users.noreply.github.com>
2021-11-13 13:54:41 -0500

    J'encule Zitix & Slushi

--- commands/Info/support.js
+++ commands/Info/support.js
@@ -3,7 +3,7 @@ const emoji = require('../../emojis.json')
 
 
 module.exports.run = async (client, message, args) => {
-    CattoEmbed(message.author, `${emoji.Sciences} Pour rejoindre le serveur support, [Cliquez-ici](https://discord.gg/kc8rxXn6Bj)`, message.channel, '')
+    CattoEmbed(message.author, `${emoji.Sciences} Pour rejoindre le serveur support, [Cliquez-ici](https://discord.gg/DhHNhND3WP)`, message.channel, '')
 }
 
 module.exports.help = {
